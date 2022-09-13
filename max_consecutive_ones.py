7eb66b94fdde947f94d54fe84242a8854fa387d5
Laurent Vauthrin <lvauthrin@gmail.com>
2022-09-13 11:23:24 -0400

    Update max_consecutive_ones.py

--- max_consecutive_ones.py
+++ max_consecutive_ones.py
@@ -1,17 +1,21 @@
-# Leetcode: https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3238/
-class max_consecutive_ones:
-    """ solution class """
-    
-    def main(self, nums: List[int]) -> int:
-        """ main_method """
-        max_ones = 0
+"""
+Leetcode: https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3238/
+"""
+from typing import List
+
+class MaxConsecutiveOnes:
+  """ solution class """
+
+  def main(self, nums: List[int]) -> int:
+    """ main_method """
+    max_ones = 0
+    counter = 0
+
+    for vals in nums:
+      if vals == 1:
+        counter += 1
+        max_ones = max(counter, max_ones)
+      else:
         counter = 0
 
-        for vals in nums:
-            if vals == 1:
-                counter += 1
-                max_ones = max(counter, max_ones)
-            else:
-                counter = 0
-
-        return max_ones
+    return max_ones
