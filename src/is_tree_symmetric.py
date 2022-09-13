621fa2cbed362b305a3eb60c1cb49717aa014211
Laurent Vauthrin <lvauthrin@gmail.com>
2022-09-13 14:59:24 -0400

    Fixed whitespace issues

--- src/is_tree_symmetric.py
+++ src/is_tree_symmetric.py
@@ -1,4 +1,4 @@
-""" 
+"""
 Leetcode: https://leetcode.com/explore/learn/card/data-structure-tree/17/solve-problems-recursively/536/
 """
 
@@ -14,38 +14,36 @@ class Node:
 
 class Solution: # pylint: disable=too-few-public-methods
   """ solution class """
-  
+
   def is_tree_symmetric_recursive(self, root: Optional[TreeNode]) -> bool:
     """ main method recursive """
     if root.left == None and root.right == None: return True
     if root.left == None or root.right == None: return False
-    
+
     return self.checkNodes(root.left, root.right)
-  
+
   def is_tree_symmetric_iterative(self, root: Optional[TreeNode]) -> bool:
     """ main method iterative """
     pass
-  
+
   def checkNodes(self, l, r):
     if l.val != r.val: return False
     result = True
-    
+
     if l.left == None and r.right != None: return False
     if l.left != None and r.right == None: return False
     if l.right == None and r.left != None: return False
     if l.right != None and r.left == None: return False
-      
+
     if l.left != None and r.right != None:
       result = result and self.checkNodes(l.left, r.right)
     if l.right != None and r.left != None:
       result = result and self.checkNodes(l.right, r.left)
-      
+
     return result
 
     def main():
       tester = Solution()
-      
+
       tree1 = Node(Node(1), Node(1))
       tester.is_tree_symmetric_recursive(tree1)
-
-
